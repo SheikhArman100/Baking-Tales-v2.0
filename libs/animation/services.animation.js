8f5754e70bb356e9b9df60c9d2fecc866eff0cc0
SheikhArman100 <sheikharman100@gmail.com>
2023-09-17 15:42:49 +0600

    service icon animation added

---
+++ libs/animation/services.animation.js
@@ -0,0 +1,12 @@
+export const iconAnimation = {
+  initial: {
+    scale: 0,
+  },
+  whileInView: {
+    scale: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
