991607e7e6b1528d257090d7e46a94eda03f4aff
Sheikh Arman <sheikharman100@gmail.com>
2023-09-19 14:34:03 +0600

    grainy effect unavailable for mobile

--- Components/GrainOverlay.js
+++ Components/GrainOverlay.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const GrainOverlay = () => {
   return (
-    <div className="h-screen w-full fixed top-0 left-0 z-50 pointer-events-none  overlay"/>
+    <div className="hidden md:flex h-screen w-full fixed top-0 left-0 z-50 pointer-events-none  overlay"/>
   );
 };
 
