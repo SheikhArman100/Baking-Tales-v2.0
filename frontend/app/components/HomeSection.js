0d21e9681cc758dc87f0c4f611fdcc2619849b59
SheikhArman100 <sheikharman100@gmail.com>
2024-01-30 00:32:25 +0600

    scroll updated

--- frontend/app/components/HomeSection.js
+++ frontend/app/components/HomeSection.js
@@ -1,20 +1,9 @@
 "use client";
 import Loader from "@/Components/Loader.js";
-import React, { useEffect, useState } from "react";
-import Lenis from "@studio-freight/lenis";
+import { useState } from "react";
 
 const HomeSection = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
 
   return (
     <main className="">
